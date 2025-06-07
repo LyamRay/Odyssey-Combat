fe38d3891022e6df1114ebd6505498aa8ef99e01
LyamRay <lyam.ray410@gmail.com>
2025-06-07 17:42:20 +0200

    Created.

---
+++ src/main/java/me/lyamray/odysseyCombat/OdysseyCombat.java
@@ -0,0 +1,19 @@
+package me.lyamray.odysseyCombat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OdysseyCombat extends JavaPlugin {
+
+
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/lyamray/odysseyCombat/utils/ChatColor.java
@@ -0,0 +1,4 @@
+package me.lyamray.odysseyCombat.utils;
+
+public class ChatColor {
+}
