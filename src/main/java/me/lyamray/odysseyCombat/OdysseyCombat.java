53adbc62ecdf30edc0966b064213d7b29ecea8f8
LyamRay <lyam.ray410@gmail.com>
2025-06-07 17:43:45 +0200

    Updated

--- src/main/java/me/lyamray/odysseyCombat/OdysseyCombat.java
+++ src/main/java/me/lyamray/odysseyCombat/OdysseyCombat.java
@@ -1,15 +1,17 @@
 package me.lyamray.odysseyCombat;
 
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OdysseyCombat extends JavaPlugin {
 
-
+    @Getter
+    private static OdysseyCombat instace;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        instace = this;
     }
 
     @Override
